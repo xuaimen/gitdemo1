2a84b5a9129d5ace9709f1e100547beef9a3104b
snake <709456276@qq.com>
2018-02-22 17:34:38 +0800

    init

---
+++ src/main/java/com/gitdemo/Test.java
@@ -0,0 +1,10 @@
+package com.gitdemo;
+
+/**
+ * Created by xuqiang on 2018/2/22.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello,git!");
+    }
+}
